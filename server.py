aa1528f979dd57c71f71fd3b9511b8c612acfa94
atrox3d <atrox3d@gmail.com>
2021-03-22 05:44:00 +0100

    setup

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import util.html, util.network
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+@util.html.h1
+@util.html.em
+def hello():
+    return "hello"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    ipaddress = util.network.get_ipaddress()
+    print(ipaddress)
+    app.run(debug=True, host=ipaddress)
